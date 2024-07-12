479ea764e0e8b276d855d4de407f4c671990f746
chunchala05 <mariatresa28@gmail.com>
2024-07-12 11:19:54 +0530

    settingsch

--- final_proj/final_proj/settings.py
+++ final_proj/final_proj/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ska_db',
         'USER': 'postgres',
-        'PASSWORD': '1475963',
+        'PASSWORD': 'Chunchala',
         'HOST': 'localhost',
         'PORT': '5432',
     }
